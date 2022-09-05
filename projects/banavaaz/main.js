8bfc8d52a1ae4b886ea93377169051838223400e
reza rastegar <85369490+rezarastegar2003@users.noreply.github.com>
2022-09-05 16:19:52 +0200

    Delete projects directory

--- projects/banavaaz/main.js
+++
@@ -1,6 +0,0 @@
-// navbar toggler - - - - -
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".navbar");
-toggler.addEventListener("click", (e) => {
-  navbar.classList.toggle("nav__expanded");
-});
--- projects/gajino/main.js
+++
@@ -1,6 +0,0 @@
-// navbar toggler - - - - -
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".header");
-toggler.addEventListener("click", (e) => {
-  navbar.classList.toggle("nav__expanded");
-});
